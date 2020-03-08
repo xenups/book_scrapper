0011535748740c6aaaa1e218e3e94fb8c0a576db
amir lesani <xenups@outlook.com>
2020-03-08 21:38:03 +0330

    add csv handler to export to csv

---
+++ file_handler/csv_handler.py
@@ -0,0 +1,7 @@
+﻿from csv import writer
+
+
+def export_book_to_csv(books):
+    thecsv = writer(open("books.csv", 'a'))
+    for book in books:
+        thecsv.writerow([str(book.title), str(book.author), str(book.publisher)])
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from fidibo.book_scrapper import BookScrapper
 from fidibo.publisher_scrapper import PublisherScrapper
-import requests
+
+from file_handler.csv_handler import export_book_to_csv
 from selenium_driver import ChromeDriverManager
 from selenium_driver import webdriver
 
 if __name__ == '__main__':
     driver = webdriver.Chrome(ChromeDriverManager().install())
+
     url = "https://fidibo.com/books/publisher"
     publishers = PublisherScrapper(url=url, driver=driver).extract_publishers()
 
     for publisher in publishers:
-        print(publisher.name)
-        books = BookScrapper(url=publisher.link, driver=driver).extract_books()
-        for book in books:
-            print(book.author)
-            print(book.title)
-
-    # url = "https://fidibo.com/books/publisher/%D9%86%D8%B4%D8%B1-%DA%86%D8%B4%D9%85%D9%87"
+        books = BookScrapper(url=publisher.link, publisher_name=publisher.name, driver=driver).extract_books()
+        export_book_to_csv(books=books)
