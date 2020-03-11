eb89124d51ed6958d59957d4f41da2e1b498e572
amir lesani <xenups@outlook.com>
2020-03-12 00:37:01 +0330

    --bookstore_handler refactored

--- bookstore_handler.py
+++ bookstore_handler.py
@@ -1,10 +1,9 @@
 ﻿from abc import ABC
-from fidibo.book_scrapper import BookScrapper as FidiboScrapper
 from bookcrawler.selenium_driver import webdriver
 from bookcrawler.selenium_driver import ChromeDriverManager
+from fidibo.book_scrapper import BookScrapper as FidiboScrapper
 from taghche.book_scrapper import BookScrapper as TaghcheScrapper
 from ketabrah.book_scrapper import BookScrapper as KetabrahScrapper
-from bookcrawler.file_handler.csv_handler import export_book_to_csv
 
 
 class BookStore(ABC):
@@ -18,22 +17,24 @@ class BookStore(ABC):
 class Fidibo(BookStore):
     def scrape_by_publishers(self):
         web_driver = webdriver.Chrome(ChromeDriverManager().install())
-        publishers_url = "https://fidibo.com/books/publisher"
-        FidiboScrapper(driver=web_driver).extract_books_by_publishers(publishers_url=publishers_url)
+        url = "https://fidibo.com/books/publisher"
+        FidiboScrapper(driver=web_driver).extract_books_by_publishers(publishers_url=url)
 
 
 class Taghche(BookStore):
     def scrape_by_category(self):
-        TaghcheScrapper(category_id=1).extract_books_by_category()
+        taghche = TaghcheScrapper()
+        taghche.set_response_count(150)
+        taghche.extract_books_api_by_category(category_id=1)
 
 
 class Ketabrah(BookStore):
     def scrape_by_publishers(self):
         publishers_url = "https://www.ketabrah.ir/page/publishers"
         web_driver = webdriver.Chrome(ChromeDriverManager().install())
-        KetabrahScrapper(driver=web_driver).extract_by_publishers(publishers_url=publishers_url)
+        KetabrahScrapper(driver=web_driver).extract_books_by_publishers(publishers_url=publishers_url)
 
     def scrape_by_category(self):
         category_url = "https://www.ketabrah.ir/book-category/%DA%A9%D8%AA%D8%A7%D8%A8%E2%80%8C%D9%87%D8%A7%DB%8C-%D8%AD%D9%82%D9%88%D9%82/"
         web_driver = webdriver.Chrome(ChromeDriverManager().install())
-        KetabrahScrapper(driver=web_driver).extract_by_category(category_url=category_url)
+        KetabrahScrapper(driver=web_driver).extract_books_by_category(category_url=category_url)
