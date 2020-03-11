f005b37cb309ae3cbb6a58bc5d33cfbe5a3021be
amir lesani <xenups@outlook.com>
2020-03-12 01:36:20 +0330

    log config added

--- bookcrawler/util.py
+++ bookcrawler/util.py
@@ -1,3 +1,7 @@
+import sys
+import logging
+
+
 class Singleton(type):
     _instances = {}
 
@@ -5,3 +9,20 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def initial_logs():
+    # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    root_logger = logging.getLogger()
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+    root_logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler(filename='mylog.log', mode='a')
+    file_handler.setLevel(level=logging.ERROR)
+    file_handler.setFormatter(log_formatter)
+    root_logger.addHandler(file_handler)
+    root_logger.setLevel(level=logging.INFO)
+    # file_handler.setLevel(level=logging.ERROR)
