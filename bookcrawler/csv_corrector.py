83afaea303155c2c7d9f08ce56a6ac22f01bd4fa
amir lesani <xenups@outlook.com>
2020-03-31 09:50:41 +0430

    csv_corrector.py refactored

--- bookcrawler/csv_corrector.py
+++ bookcrawler/csv_corrector.py
@@ -6,29 +6,53 @@ class CSVCorrectorAbstract(ABC):
     def __init__(self, file_path):
         self._data_frame = pd.read_csv(file_path, skipinitialspace=True)
 
-    def remove_blank_columns(self):
-        # self._data_frame = self._data_frame[self._data_frame.columns.dropna()]
+    def drop_blank_columns(self):
         self._data_frame.dropna(how='all', axis=1, inplace=True)
-        self._data_frame.to_csv('ketabrah.csv', encoding='utf-8', index=False)
 
-    def add_store_name(self):
-        pass
+    def clean_price(self):
+        self._data_frame['price'].fillna(0, inplace=True)
+        self._data_frame['price'] = self._data_frame['price'].astype(str).str.replace(r'\D+', '').astype(float)
 
-    def convert_price(self):
-        pass
+    def drop_column(self, column_name: str):
+        self._data_frame.drop(column_name, axis=1, inplace=True)
 
     def remove_duplicate_lines(self):
         self._data_frame.drop_duplicates(inplace=True)
-        self._data_frame.to_csv('ketabrah.csv')
 
+    def export_to_csv(self, file_name="output"):
+        self._data_frame.to_csv(file_name, index=False, encoding='utf-8')
 
-class NavarCSVCorrector(CSVCorrectorAbstract):
-    def convert_price(self):
+    def correct_data(self):
         pass
 
 
+class NavarCSVCorrector(CSVCorrectorAbstract):
+    def correct_data(self):
+        self.drop_blank_columns()
+        self.remove_duplicate_lines()
+        self.clean_price()
+        self.export_to_csv("cleaned_navar.csv")
+
+
 class KetabrahCSVCorrector(CSVCorrectorAbstract):
-    def convert_price(self):
-        self._data_frame['price'].fillna(0, inplace=True)
-        self._data_frame['price'] = self._data_frame['price'].astype(str).str.replace(r'\D+', '').astype(float)
-        self._data_frame.to_csv('ketabrah.csv', index=False)
+    def correct_data(self):
+        self.drop_blank_columns()
+        self.remove_duplicate_lines()
+        self.clean_price()
+        self.export_to_csv("cleaned_ketabrah.csv")
+
+
+class TaghcheCSVCorrector(CSVCorrectorAbstract):
+    def correct_data(self):
+        self.drop_blank_columns()
+        self.remove_duplicate_lines()
+        self.export_to_csv("cleaned_taghche.csv")
+
+
+class FidiboCSVCorrector(CSVCorrectorAbstract):
+    def correct_data(self):
+        self.drop_blank_columns()
+        self.drop_column("url")
+        self.remove_duplicate_lines()
+        self.clean_price()
+        self.export_to_csv("cleaned_fidibo.csv")
