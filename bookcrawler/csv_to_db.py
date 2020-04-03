ba1616d712b43c5091e03771f772f2923b89a062
amir lesani <xenups@outlook.com>
2020-04-03 10:44:46 +0430

    using dotenv to connect database

--- bookcrawler/csv_to_db.py
+++ bookcrawler/csv_to_db.py
@@ -2,21 +2,26 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+from settings import database
+
 
 class CSVToDB(object):
-    def __init__(self, csv_input, db_name, db_address, port, table_name):
-        self._db_name = db_name
-        self._db_address = db_address
-        self._db_port = port
+    def __init__(self, csv_input, table_name):
+        self._db_name = database["db_name"]
+        self._db_user = database["db_user"]
+        self._db_pass = database["db_pass"]
+        self._db_host = database["db_host"]
+        self._db_port = database["db_port"]
         self._csv_input = csv_input
         self._table_name = table_name
 
     def convert_to_postgres(self, ):
         try:
-            _engine = create_engine(
-                'postgresql://{}@{}:{}/{}'.format(self._db_name, self._db_address, self._db_port, self._table_name))
+            _engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(self._db_user, self._db_pass,
+                                                                         self._db_host, self._db_port,
+                                                                         self._db_name))
             df = pd.read_csv(self._csv_input)
-            df.to_sql("fidibo", _engine)
+            df.to_sql(self._table_name, _engine, if_exists="replace")
         except Exception as e:
             raise e
 
