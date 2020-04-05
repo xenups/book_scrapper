b5d088f254c7ada9bde3171de497fd3bfb7522b7
amir lesani <xenups@outlook.com>
2020-04-05 14:49:32 +0430

    scheduler added to manage crawling

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import logging
+import time
+import schedule
 from bookcrawler.util import initialize_logs
 from bookstore_handler import Fidibo, Taghche, Ketabrah, Navar
 
+
+def crawling():
+    try:
+        Fidibo().crawl_by_publishers()
+        Taghche().crawl_by_category()
+        Ketabrah().crawl_by_publishers()
+        Navar().crawl_by_category()
+    except Exception as ex:
+        logging.error(ex)
+
+
 if __name__ == '__main__':
     initialize_logs()
-    Fidibo().crawl_by_publishers()
-    # Taghche().crawl_by_publishers()
-    # Taghche().crawl_by_category()
-    # Ketabrah().crawl_by_category()
-    # Ketabrah().crawl_by_publishers()
-    # Navar().crawl_by_category()
+    schedule.every(1).minutes.do(crawling)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
