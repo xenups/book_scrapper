1913ee8f47a0e5780e78c07c12d12e0cc8b15895
amir lesani <xenups@outlook.com>
2020-04-05 14:45:37 +0430

    display removed clean_csv_files() added to clear root before start

--- bookstore_handler.py
+++ bookstore_handler.py
@@ -3,6 +3,7 @@ from abc import ABC
 from pyvirtualdisplay import Display
 from bookcrawler.csv_to_db import CSVToDB
 from bookcrawler.csv_corrector import CSVCorrectorFactory
+from bookcrawler.util import clean_csv_files
 from navar.book_scrapper import BookScrapper as NavarScrapper
 from fidibo.book_scrapper import BookScrapper as FidiboScrapper
 from taghche.book_scrapper import BookScrapper as TaghcheScrapper
@@ -12,7 +13,7 @@ from settings import fidibo_worker, taghche_response_count, display_visibility
 
 class BookStore(ABC):
     def __init__(self):
-        self.display = Display(visible=display_visibility, size=(800, 600))
+        clean_csv_files()
 
     def crawl_by_publishers(self):
         pass
@@ -23,16 +24,11 @@ class BookStore(ABC):
 
 class Fidibo(BookStore):
     def crawl_by_publishers(self):
-        self.display.start()
         csv_path = FidiboScrapper(without_browser=True).multi_book_extractor_by_publishers_url(
             worker=int(fidibo_worker))
         cleaned_data = CSVCorrectorFactory().correct_data("FidiboCorrector", csv_path)
-        CSVToDB(csv_input=cleaned_data, table_name="fidibo_test").convert_to_postgres()
+        CSVToDB(csv_input=cleaned_data, table_name="fidibo").convert_to_postgres()
         logging.info("Task successfully finished ...")
-        self.display.stop()
-
-    def __del__(self):
-        self.display.stop()
 
 
 class Taghche(BookStore):
@@ -40,41 +36,30 @@ class Taghche(BookStore):
         taghche = TaghcheScrapper()
         taghche.set_response_count(int(taghche_response_count))
         csv_path = taghche.extract_books_api_by_category(category_id=1)
+        # csv_path = taghche.extract_books_api_by_category(category_id=115)
         cleaned_data = CSVCorrectorFactory().correct_data("TaghcheCorrector", csv_path)
-        CSVToDB(csv_input=cleaned_data, table_name="fidibo_test").convert_to_postgres()
+        CSVToDB(csv_input=cleaned_data, table_name="taghche").convert_to_postgres()
         logging.info("Task successfully finished ...")
 
 
 class Ketabrah(BookStore):
     def crawl_by_publishers(self):
-        self.display.start()
-        csv_path = KetabrahScrapper(without_browser=False, optimized_mode=True).extract_books_by_publishers()
+        csv_path = KetabrahScrapper(without_browser=True, optimized_mode=True).extract_books_by_publishers()
         cleaned_data = CSVCorrectorFactory().correct_data("KetabrahCorrector", csv_path)
-        CSVToDB(csv_input=cleaned_data, table_name="ketabrah")
+        CSVToDB(csv_input=cleaned_data, table_name="ketabrah").convert_to_postgres()
         logging.info("Task successfully finished ...")
-        self.display.stop()
 
     def crawl_by_category(self):
-        self.display.start()
-        csv_path = KetabrahScrapper(without_browser=False, optimized_mode=True).extract_books_by_category()
+        csv_path = KetabrahScrapper(without_browser=True, optimized_mode=True).extract_books_by_category()
         cleaned_data = CSVCorrectorFactory().correct_data("KetabrahCorrector", csv_path)
         CSVToDB(csv_input=cleaned_data, table_name="ketabrah_by_category").convert_to_postgres()
         logging.info("Task successfully finished ...")
-        self.display.stop()
-
-    def __del__(self):
-        self.display.stop()
 
 
 class Navar(BookStore):
     def crawl_by_category(self):
-        self.display.start()
-        navar = NavarScrapper(optimized_mode=False, without_browser=False)
+        navar = NavarScrapper(optimized_mode=True, without_browser=True)
         csv_path = navar.extract_books()
         cleaned_data = CSVCorrectorFactory().correct_data("NavarCorrector", csv_path)
         CSVToDB(csv_input=cleaned_data, table_name="navar").convert_to_postgres()
         logging.info("Task successfully finished ...")
-        self.display.stop()
-
-    def __del__(self):
-        self.display.stop()
