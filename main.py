51cf7a1481e4e8024cafcccfc6ea33f000b3ba27
amir lesani <xenups@outlook.com>
2020-04-06 05:50:36 +0430

    every thursday crawling will be start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
 import time
+import logging
 import schedule
 from bookcrawler.util import initialize_logs
 from bookstore_handler import Fidibo, Taghche, Ketabrah, Navar
@@ -7,17 +7,17 @@ from bookstore_handler import Fidibo, Taghche, Ketabrah, Navar
 
 def crawling():
     try:
-        Fidibo().crawl_by_publishers()
         Taghche().crawl_by_category()
         Ketabrah().crawl_by_publishers()
         Navar().crawl_by_category()
+        Fidibo().crawl_by_publishers()
     except Exception as ex:
         logging.error(ex)
 
 
 if __name__ == '__main__':
     initialize_logs()
-    schedule.every(1).minutes.do(crawling)
+    schedule.every().thursday.at("04:00").do(crawling)
     while True:
         schedule.run_pending()
         time.sleep(1)
